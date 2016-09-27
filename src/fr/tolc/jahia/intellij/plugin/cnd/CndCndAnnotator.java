862579b61f91758b8ab5107f24b9a5d300fbabc4
Tolc <thomas.coquel117@gmail.com>
2016-09-27 15:02:11 +0200

    Node type files creation quick fix done

--- src/fr/tolc/jahia/intellij/plugin/cnd/CndCndAnnotator.java
+++ src/fr/tolc/jahia/intellij/plugin/cnd/CndCndAnnotator.java
@@ -3,7 +3,10 @@ package fr.tolc.jahia.intellij.plugin.cnd;
 import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import fr.tolc.jahia.intellij.plugin.cnd.psi.CndTypes;
@@ -16,16 +19,33 @@ public class CndCndAnnotator implements Annotator {
         if (CndTypes.NODE_TYPE_NAME.equals(element.getNode().getElementType())) {
             String nodeTypeName = element.getText();
 
-            PsiElement namespaceEl = element.getPrevSibling().getPrevSibling();
-            if (CndTypes.NODE_TYPE_NAMESPACE.equals(namespaceEl.getNode().getElementType())) {
-                String namespace = namespaceEl.getText();
+            PsiElement nextSibling = element.getNextSibling();
+            if (nextSibling != null && CndTypes.NODE_TYPE_DECLARATION_CLOSING.equals(nextSibling.getNode().getElementType())) { //Make sure this is a node type declaration, and not use
+                
+                PsiElement colonEl = element.getPrevSibling();
+                if (colonEl != null && CndTypes.NODE_TYPE_DECLARATION_COLON.equals(colonEl.getNode().getElementType())) {
 
-                Project project = element.getProject();
-                VirtualFile nodeTypeFolder = project.getBaseDir().findFileByRelativePath("src/main/webapp/" + namespace + "_" + nodeTypeName);
-                if (nodeTypeFolder == null) {
-                    Annotation annotation = holder.createInfoAnnotation(element.getTextRange(), "Node type " + namespace + ":" + nodeTypeName + " does not have any associated files");
-                    annotation.setTextAttributes(CndSyntaxHighlighter.NODE_TYPE);
-                    annotation.registerFix(new CreateNodeTypeFilesQuickFix(namespace, nodeTypeName));
+                    PsiElement namespaceEl = colonEl.getPrevSibling();
+                    if (namespaceEl != null && CndTypes.NODE_TYPE_NAMESPACE.equals(namespaceEl.getNode().getElementType())) {
+                        String namespace = namespaceEl.getText();
+
+                        Project project = element.getProject();
+                        Module currentModule = ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(element.getContainingFile().getVirtualFile());
+                        VirtualFile[] sourceRoots = ModuleRootManager.getInstance(currentModule).getSourceRoots();
+
+                        VirtualFile nodeTypeFolder = null;
+                        for (VirtualFile sourceRoot : sourceRoots) {
+                            nodeTypeFolder = sourceRoot.findFileByRelativePath("main/webapp/" + namespace + "_" + nodeTypeName);
+                            if (nodeTypeFolder != null) {
+                                break;
+                            }
+                        }
+                        if (nodeTypeFolder == null) {
+                            Annotation annotation = holder.createInfoAnnotation(element.getTextRange(), "Node type " + namespace + ":" + nodeTypeName + " does not have any associated files");
+                            annotation.setTextAttributes(CndSyntaxHighlighter.NODE_TYPE);
+                            annotation.registerFix(new CreateNodeTypeFilesQuickFix(namespace, nodeTypeName));
+                        }
+                    }
                 }
             }
         }
--- src/fr/tolc/jahia/intellij/plugin/cnd/CreateNodeTypeFilesQuickFix.java
+++ src/fr/tolc/jahia/intellij/plugin/cnd/CreateNodeTypeFilesQuickFix.java
@@ -1,31 +1,25 @@
 package fr.tolc.jahia.intellij.plugin.cnd;
 
+import java.io.File;
+import java.io.IOException;
+
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
-import com.intellij.lang.ASTNode;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.pom.Navigatable;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.search.FileTypeIndex;
-import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.IncorrectOperationException;
-import com.intellij.util.indexing.FileBasedIndex;
-import fr.tolc.jahia.intellij.plugin.cnd.psi.CndElementFactory;
-import fr.tolc.jahia.intellij.plugin.cnd.psi.CndFile;
-import fr.tolc.jahia.intellij.plugin.cnd.psi.CndNodeType;
-import fr.tolc.jahia.intellij.plugin.cnd.psi.CndTypes;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
 
 public class CreateNodeTypeFilesQuickFix extends BaseIntentionAction {
 
@@ -60,50 +54,51 @@ public class CreateNodeTypeFilesQuickFix extends BaseIntentionAction {
         ApplicationManager.getApplication().invokeLater(new Runnable() {
             @Override
             public void run() {
-                //TODO
-//                Collection<VirtualFile> virtualFiles = FileBasedIndex.getInstance().getContainingFiles(FileTypeIndex.NAME, CndFileType.INSTANCE, GlobalSearchScope.allScope(project));
-//                if (virtualFiles.size() == 1) {
-//                    createNodeType(project, virtualFiles.iterator().next());
-//                } else {
-//                    final FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFileDescriptor(CndFileType.INSTANCE);
-//                    descriptor.setRoots(project.getBaseDir());
-//                    final VirtualFile file = FileChooser.chooseFile(descriptor, project, null);
-//                    if (file != null) {
-//                        createNodeType(project, file);
-//                    }
-//                }
+                String finalDirectory = "main/webapp/" + namespace + "_" + nodeTypeName;
+
+                Module currentModule = ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(file.getVirtualFile());
+                VirtualFile[] sourceRoots = ModuleRootManager.getInstance(currentModule).getSourceRoots();
+
+                if (sourceRoots.length == 1) {
+                    createNodeTypeFiles(project, sourceRoots[0], finalDirectory);
+                } else {
+                    //TODO: only allow to choose between source folders
+                    final FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
+                    descriptor.setTitle("Select Root Folder");
+                    descriptor.setRoots(sourceRoots);
+                    final VirtualFile file = FileChooser.chooseFile(descriptor, project, null);
+                    if (file != null) {
+                        createNodeTypeFiles(project, file, finalDirectory);
+                    }
+                }
             }
         });
     }
+    
+    private void createNodeTypeFiles(final Project project, final VirtualFile sourceDirectory, final String directory) {
+        File folder = new File(sourceDirectory.getCanonicalPath(), directory);
+        if(!folder.exists()) {
+            folder.mkdirs();
+        }
+        VirtualFile nodeTypeFolder = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(folder);
 
-//    private void createNodeType(final Project project, final VirtualFile file) {
-//        new WriteCommandAction.Simple(project) {
-//            @Override
-//            public void run() {
-//                CndFile cndFile = (CndFile) PsiManager.getInstance(project).findFile(file);
-//                ASTNode lastChildNode = cndFile.getNode().getLastChildNode();
-//                ASTNode beforeLastChildNode = lastChildNode.getTreePrev();
-//
-//                //There should be an empty line between two node type declarations
-//                if (!lastChildNode.getElementType().equals(CndTypes.CRLF)) {
-//                    cndFile.getNode().addChild(CndElementFactory.createCRLF(project).getNode());
-//                }
-//                if (!beforeLastChildNode.getElementType().equals(CndTypes.CRLF)) {
-//                    cndFile.getNode().addChild(CndElementFactory.createCRLF(project).getNode());
-//                }
-//
-//                CndNodeType cndNodeType = CndElementFactory.createNodeType(project, nodeTypeName, namespace);
-//                cndFile.getNode().addChild(cndNodeType.getNode());
-//                ((Navigatable) cndNodeType.getLastChild().getNavigationElement()).navigate(true);
-//                FileEditorManager.getInstance(project).getSelectedTextEditor().getCaretModel().moveCaretRelatively(2, 0, false, false, false);
+        File jsp = new File(nodeTypeFolder.getCanonicalPath(),nodeTypeName + ".jsp");
+        File properties = new File(nodeTypeFolder.getCanonicalPath(),nodeTypeName + ".properties");
+        try {
+            jsp.createNewFile();
+            properties.createNewFile();
+        } catch (IOException e) {
+            throw new IncorrectOperationException(e);
+        }
 
-                // almost the same thing but manipulating plain text of the document instead of PSI
-                //                FileEditorManager.getInstance(project).openFile(file, true);
-                //                final Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-                //                final Document document = editor.getDocument();
-                //                document.insertString(document.getTextLength(), "\n" + key.replaceAll(" ", "\\\\ ") + " = ");
-                //                editor.getCaretModel().getPrimaryCaret().moveToOffset(document.getTextLength());
-//            }
-//        }.execute();
-//    }
+        //TODO: default content
+        
+        VirtualFile jspFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(jsp);
+        FileEditorManager.getInstance(project).openFile(jspFile, true);
+
+        VirtualFile propertiesFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(properties);
+        FileEditorManager.getInstance(project).openFile(propertiesFile, false);
+
+        //TODO: expand new folder in project explorer
+    }
 }
