3c1339bf1f6ddd851639a67038acc1d506b170ac
Tolc <thomas.coquel117@gmail.com>
2016-11-01 00:16:35 +0100

    Embedding Jahia base cnd files done Creating a jar containing the cnd files, and adding it to the libraries of the modules containing a cnd file

--- src/fr/tolc/jahia/intellij/plugin/cnd/CndProjectComponent.java
+++ src/fr/tolc/jahia/intellij/plugin/cnd/CndProjectComponent.java
@@ -4,15 +4,13 @@
  */
 package fr.tolc.jahia.intellij.plugin.cnd;
 
-import java.io.File;
-import java.util.Collection;
-
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryTable;
@@ -20,15 +18,21 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.indexing.FileBasedIndex;
+import fr.tolc.jahia.intellij.plugin.cnd.utils.CndProjectFilesUtil;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Collection;
 
 public class CndProjectComponent implements ProjectComponent {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(CndProjectComponent.class);
     
-    private static final String JAHIA_PLUGIN_LIBRARY_NAME = "jahia-base-and-modules-cnd-files";
+    private static final String JAHIA_PLUGIN_LIBRARY_NAME = "jahia-plugin-base-cnd-files";
+    private static final String JAHIA_RESOURCES_FOLDER = "jahia";
+    private static final String JAHIA_CND_JAR_NAME = "jahia-plugin-cnds.jar";
 
     private Project project;
 
@@ -36,14 +40,10 @@ public class CndProjectComponent implements ProjectComponent {
         this.project = project;
     }
 
+    @NotNull
     @Override
-    public void initComponent() {
-      
-    }
-
-    @Override
-    public void disposeComponent() {
-
+    public String getComponentName() {
+        return "CndProjectComponent";
     }
 
     @Override
@@ -54,36 +54,47 @@ public class CndProjectComponent implements ProjectComponent {
                 ApplicationManager.getApplication().runWriteAction(new Runnable() {
                     @Override
                     public void run() {
-                        Collection<VirtualFile> virtualFiles = FileBasedIndex.getInstance().getContainingFiles(FileTypeIndex.NAME, CndFileType.INSTANCE, GlobalSearchScope.allScope(project));
+                        File jahiaResourcesFolder = CndProjectFilesUtil.getResourceFile(JAHIA_RESOURCES_FOLDER);
 
-                        for (VirtualFile virtualFile : virtualFiles) {
+                        if (jahiaResourcesFolder.exists() && jahiaResourcesFolder.isDirectory()) {
+                            //Re-generate 'fake' jar containing cnd files
+                            File jarFile = CndProjectFilesUtil.getResourceFile(JAHIA_RESOURCES_FOLDER + "/" + JAHIA_CND_JAR_NAME);
+                            if (jarFile != null && jarFile.exists()) {
+                                jarFile.delete();
+                            }
                             try {
-                                Module fileModule = ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(virtualFile);
-
-                                if (fileModule != null) {
-                                    final ModifiableRootModel rootModel = ModuleRootManager.getInstance(fileModule).getModifiableModel();
-                                    LibraryTable.ModifiableModel moduleLibraryTable = rootModel.getModuleLibraryTable().getModifiableModel();
-
-                                    if (moduleLibraryTable.getLibraryByName(JAHIA_PLUGIN_LIBRARY_NAME) == null) {
-                                        Library library = moduleLibraryTable.createLibrary(JAHIA_PLUGIN_LIBRARY_NAME);
-
-                                        String librayFolderPath = this.getClass().getClassLoader().getResource("jahia").getFile();
-                                        File librayFolder = new File(librayFolderPath);
-
-                                        Library.ModifiableModel modifiableModel = library.getModifiableModel();
-                                        //                        for (OrderRootType rootType : OrderRootType.getAllTypes()) {
-                                        //                            modifiableModel.addRoot(librayFolder.getAbsolutePath(), rootType);
-                                        //                        }
-                                        //                        modifiableModel.addRoot(librayFolder.getAbsolutePath(), OrderRootType.SOURCES);
-                                        modifiableModel.addJarDirectory(librayFolder.getAbsolutePath(), false);
-                                        modifiableModel.commit();
-                                        moduleLibraryTable.commit();
-                                        //                    ModuleRootModificationUtil.updateModel()
-                                        rootModel.commit();
-                                    }
-                                }
+                                CndProjectFilesUtil.fileToJar(jahiaResourcesFolder, jahiaResourcesFolder.getAbsolutePath() + "/" + JAHIA_CND_JAR_NAME, "cnd");
                             } catch (Exception e) {
-                                LOGGER.warn("Error while adding Jahia CND files to module(s) libraries", e);
+                               LOGGER.warn("Error generating Jahia base cnd files 'fake' jar", e);
+                            }
+
+
+                            //Adding it to the modules libraries
+                            Collection<VirtualFile> virtualFiles = FileBasedIndex.getInstance().getContainingFiles(FileTypeIndex.NAME, CndFileType.INSTANCE, GlobalSearchScope.allScope(project));
+                            for (VirtualFile virtualFile : virtualFiles) {
+                                try {
+                                    Module fileModule = ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(virtualFile);
+
+                                    if (fileModule != null) {
+                                        final ModifiableRootModel rootModel = ModuleRootManager.getInstance(fileModule).getModifiableModel();
+                                        LibraryTable.ModifiableModel moduleLibraryTable = rootModel.getModuleLibraryTable().getModifiableModel();
+
+                                        if (moduleLibraryTable.getLibraryByName(JAHIA_PLUGIN_LIBRARY_NAME) == null) {
+                                            Library library = moduleLibraryTable.createLibrary(JAHIA_PLUGIN_LIBRARY_NAME);
+
+                                            String libraryFolderPath = this.getClass().getClassLoader().getResource(JAHIA_RESOURCES_FOLDER + "/" + JAHIA_CND_JAR_NAME).getFile();
+                                            File libraryFolder = new File(libraryFolderPath);
+
+                                            Library.ModifiableModel modifiableModel = library.getModifiableModel();
+                                            modifiableModel.addRoot("jar://" + libraryFolder.getAbsolutePath() + "!/", OrderRootType.CLASSES);
+                                            modifiableModel.commit();
+                                            moduleLibraryTable.commit();
+                                            rootModel.commit();
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    LOGGER.warn("Error while adding Jahia CND files to module(s) libraries", e);
+                                }
                             }
                         }
                     }
@@ -93,13 +104,17 @@ public class CndProjectComponent implements ProjectComponent {
     }
 
     @Override
-    public void projectClosed() {
+    public void initComponent() {
 
     }
 
-    @NotNull
     @Override
-    public String getComponentName() {
-        return "CndProjectComponent";
+    public void disposeComponent() {
+
+    }
+
+    @Override
+    public void projectClosed() {
+
     }
 }
