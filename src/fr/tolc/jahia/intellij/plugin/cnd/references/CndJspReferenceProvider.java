cba3b694a39f766108d053b57b5a9fc1ff0e375d
Tolc <thomas.coquel117@gmail.com>
2017-11-17 09:59:45 +0100

    Better nodetypes reference and completion in JSP Nodetypes are now recognized in EL expressions and tag text

--- src/fr/tolc/jahia/intellij/plugin/cnd/references/CndJspReferenceProvider.java
+++ src/fr/tolc/jahia/intellij/plugin/cnd/references/CndJspReferenceProvider.java
@@ -1,35 +1,26 @@
 package fr.tolc.jahia.intellij.plugin.cnd.references;
 
-import com.intellij.lang.properties.psi.impl.PropertyKeyImpl;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiLiteralExpression;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiReferenceProvider;
-import com.intellij.psi.jsp.el.ELElementTypes;
-import com.intellij.psi.xml.XmlAttributeValue;
-import com.intellij.psi.xml.XmlElementType;
-import com.intellij.psi.xml.XmlToken;
-import com.intellij.util.ProcessingContext;
-import com.intellij.util.containers.HashSet;
-import fr.tolc.jahia.intellij.plugin.cnd.model.NodeTypeModel;
-import fr.tolc.jahia.intellij.plugin.cnd.model.PropertiesFileCndKeyModel;
-import fr.tolc.jahia.intellij.plugin.cnd.psi.CndExtension;
-import fr.tolc.jahia.intellij.plugin.cnd.psi.CndSubNodeDefaultType;
-import fr.tolc.jahia.intellij.plugin.cnd.psi.CndSubNodeType;
-import fr.tolc.jahia.intellij.plugin.cnd.psi.CndSuperType;
-import fr.tolc.jahia.intellij.plugin.cnd.references.types.CndNamespaceIdentifierReference;
-import fr.tolc.jahia.intellij.plugin.cnd.references.types.CndNodeTypeIdentifierReference;
-import fr.tolc.jahia.intellij.plugin.cnd.references.types.CndPropertyIdentifierReference;
-import fr.tolc.jahia.intellij.plugin.cnd.utils.PsiUtil;
-import org.jetbrains.annotations.NotNull;
+import static fr.tolc.jahia.intellij.plugin.cnd.model.NodeTypeModel.nodeTypeGlobalRegex;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 
-import static fr.tolc.jahia.intellij.plugin.cnd.model.NodeTypeModel.nodeTypeGlobalRegex;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiReferenceProvider;
+import com.intellij.psi.jsp.el.ELElementTypes;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.psi.xml.XmlElementType;
+import com.intellij.util.ProcessingContext;
+import com.intellij.util.containers.HashSet;
+import fr.tolc.jahia.intellij.plugin.cnd.model.NodeTypeModel;
+import fr.tolc.jahia.intellij.plugin.cnd.references.types.CndNamespaceIdentifierReference;
+import fr.tolc.jahia.intellij.plugin.cnd.references.types.CndNodeTypeIdentifierReference;
+import fr.tolc.jahia.intellij.plugin.cnd.utils.PsiUtil;
+import org.jetbrains.annotations.NotNull;
 
 public class CndJspReferenceProvider extends PsiReferenceProvider {
 
@@ -63,13 +54,13 @@ public class CndJspReferenceProvider extends PsiReferenceProvider {
                         }
 
                         if (nodeTypeModel != null) {
-                            int offset = matcher.start();
+                            int offset = (psiElement.getTextRange().getStartOffset() - element.getTextRange().getStartOffset()) + matcher.start();
                             String namespace = nodeTypeModel.getNamespace();
                             String nodeTypeName = nodeTypeModel.getNodeTypeName();
 
                             //Text ranges here are relative!!
-                            psiReferences.add(new CndNamespaceIdentifierReference(psiElement, new TextRange(offset, namespace.length() + offset), namespace));
-                            psiReferences.add(new CndNodeTypeIdentifierReference(psiElement, new TextRange(namespace.length() + offset + 1, group.length() + offset), namespace, nodeTypeName));
+                            psiReferences.add(new CndNamespaceIdentifierReference(element, new TextRange(offset, namespace.length() + offset), namespace));
+                            psiReferences.add(new CndNodeTypeIdentifierReference(element, new TextRange(namespace.length() + offset + 1, group.length() + offset), namespace, nodeTypeName));
                         }
                     }
 
