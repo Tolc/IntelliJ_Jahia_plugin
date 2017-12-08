95e936be65b7938042a336efd9523cf3de3083ba
Tolc <thomas.coquel117@gmail.com>
2017-12-08 17:46:44 +0100

    Fixed Jahia Tool Window condition

--- src/fr/tolc/jahia/intellij/plugin/cnd/toolWindow/JahiaToolWindowCondition.java
+++ src/fr/tolc/jahia/intellij/plugin/cnd/toolWindow/JahiaToolWindowCondition.java
@@ -1,6 +1,8 @@
 package fr.tolc.jahia.intellij.plugin.cnd.toolWindow;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.Condition;
 import fr.tolc.jahia.intellij.plugin.cnd.utils.CndProjectFilesUtil;
 
@@ -8,6 +10,12 @@ public class JahiaToolWindowCondition implements Condition<Project> {
 
     @Override
     public boolean value(Project project) {
-        return CndProjectFilesUtil.getProjectCndFiles(project).size() > 0;
+        Boolean displayToolWindow = ApplicationManager.getApplication().runReadAction(new Computable<Boolean>() {
+            @Override
+            public Boolean compute() {
+                return CndProjectFilesUtil.getProjectCndFiles(project).size() > 0;
+            }
+        });
+        return (displayToolWindow != null)? displayToolWindow : false;
     }
 }
