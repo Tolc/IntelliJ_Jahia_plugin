ecc9aa16ceb27bb15f9b81556795693383c90bbb
Tolc <thomas.coquel117@gmail.com>
2017-12-08 17:46:19 +0100

    Custom edit mode icons (in 'icons' folder) displayed in the gutter in CND files next to the corresponding nodetype/mixin

--- src/fr/tolc/jahia/intellij/plugin/cnd/lineMarker/CndCndLineMarkerProvider.java
+++ src/fr/tolc/jahia/intellij/plugin/cnd/lineMarker/CndCndLineMarkerProvider.java
@@ -1,14 +1,22 @@
 package fr.tolc.jahia.intellij.plugin.cnd.lineMarker;
 
+import java.io.File;
+import java.net.URL;
 import java.util.Collection;
+
+import javax.swing.*;
 
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.openapi.util.IconLoader;
 import com.intellij.psi.PsiElement;
 import fr.tolc.jahia.intellij.plugin.cnd.icons.CndIcons;
 import fr.tolc.jahia.intellij.plugin.cnd.psi.CndNamespace;
 import fr.tolc.jahia.intellij.plugin.cnd.psi.CndNodeType;
+import fr.tolc.jahia.intellij.plugin.cnd.utils.CndProjectFilesUtil;
+import fr.tolc.jahia.intellij.plugin.cnd.utils.CndTranslationUtil;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class CndCndLineMarkerProvider extends RelatedItemLineMarkerProvider {
@@ -27,6 +35,29 @@ public class CndCndLineMarkerProvider extends RelatedItemLineMarkerProvider {
             CndNodeType cndNodeType = (CndNodeType) element;
 
             NavigationGutterIconBuilder<PsiElement> builder;
+
+
+            //Custom icon
+            String jahiaWorkFolderPath = CndProjectFilesUtil.getJahiaWorkFolderPath(element);
+            if (StringUtils.isNotBlank(jahiaWorkFolderPath)) {
+                String iconPath = jahiaWorkFolderPath + "/icons/" + CndTranslationUtil.convertNodeTypeIdentifierToPropertyName(cndNodeType.getNodeTypeNamespace(), cndNodeType.getNodeTypeName()) + ".png";
+                File iconFile = new File(iconPath);
+                if (iconFile.exists()) {
+                    try {
+                        Icon icon = IconLoader.findIcon(new URL("file:/" + iconPath));
+                        if (icon != null) {
+                            builder = NavigationGutterIconBuilder.create(icon)
+                                    .setTarget(null)
+                                    .setTooltipText("Node type " + cndNodeType.getNodeTypeNamespace() + ":" + cndNodeType.getNodeTypeName() + " custom icon");
+                            result.add(builder.createLineMarkerInfo(element));
+                        }
+                    } catch (Exception e) {
+                        //Nothing to do
+                    }
+                }
+            }
+            
+            
             if (cndNodeType.isMixin()) {
                  builder = NavigationGutterIconBuilder.create(CndIcons.MIXIN)
                         .setTarget(null)
