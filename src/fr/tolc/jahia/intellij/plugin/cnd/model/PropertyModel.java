75c28ea314e06a8719e176bdb17cf3600b0142e9
Tolc <thomas.coquel117@gmail.com>
2017-12-13 16:43:59 +0100

    JSP Properties colors and references for "currentNode.properties.propName" and "node.properties['propName']" formats

--- src/fr/tolc/jahia/intellij/plugin/cnd/model/PropertyModel.java
+++ src/fr/tolc/jahia/intellij/plugin/cnd/model/PropertyModel.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2017 by Bank Lombard Odier & Co Ltd, Geneva, Switzerland. This software is subject
- * to copyright protection under the laws of Switzerland and other countries. ALL RIGHTS RESERVED.
- */
 package fr.tolc.jahia.intellij.plugin.cnd.model;
 
 import java.util.regex.Pattern;
@@ -11,12 +7,41 @@ public class PropertyModel {
     private static final String PROPERTY_PART = "([A-Za-z](?:[A-Za-z0-9:.]*[A-Za-z0-9])?)";
     public static final String CURRENT_NODE = "currentNode";
     private static final String PROPERTIES_MAP_PART = "\\.properties";
-    private static final String BEFORE_PART = "([^\\s.]+)";
-    
+    private static final String BEFORE_PART = "([^\\s.{]+)";
+
     private static final String PROPERTY_GET_SIMPLE = BEFORE_PART + PROPERTIES_MAP_PART + "\\." + PROPERTY_PART_SIMPLE;
     private static final String PROPERTY_GET_BRACKETS = BEFORE_PART + PROPERTIES_MAP_PART + "\\[['\"]" + PROPERTY_PART + "['\"]]";
 
     public static final Pattern propertyGetRegexSimple = Pattern.compile(PROPERTY_GET_SIMPLE);
     public static final Pattern propertyGetRegexBrackets = Pattern.compile(PROPERTY_GET_BRACKETS);
     public static final Pattern propertyGetRegex = Pattern.compile(PROPERTY_GET_SIMPLE + "|" + PROPERTY_GET_BRACKETS);
+
+    /**
+     * Properties that don't need to be defined on a node to be usable
+     */
+    public static final String[] UNIVERSAL_PROPERTIES = {"j:defaultCategory"};
+
+    private NodeTypeModel nodeType;
+    private String propertyName;
+
+    public PropertyModel(NodeTypeModel nodeType, String propertyName) {
+        this.nodeType = nodeType;
+        this.propertyName = propertyName;
+    }
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    public NodeTypeModel getNodeType() {
+        return nodeType;
+    }
+
+    public void setNodeType(NodeTypeModel nodeType) {
+        this.nodeType = nodeType;
+    }
 }
