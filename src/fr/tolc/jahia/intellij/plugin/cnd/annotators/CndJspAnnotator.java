20e964f670c21021184006416a5f1ced9be791d0
Tolc <thomas.coquel117@gmail.com>
2017-12-20 19:16:21 +0100

    <jcr:nodeProperty/> color and reference/completion

--- src/fr/tolc/jahia/intellij/plugin/cnd/annotators/CndJspAnnotator.java
+++ src/fr/tolc/jahia/intellij/plugin/cnd/annotators/CndJspAnnotator.java
@@ -15,7 +15,10 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.jsp.el.ELElementTypes;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlElementType;
+import com.intellij.psi.xml.XmlTag;
 import fr.tolc.jahia.intellij.plugin.cnd.CndSyntaxHighlighter;
 import fr.tolc.jahia.intellij.plugin.cnd.model.NodeTypeModel;
 import fr.tolc.jahia.intellij.plugin.cnd.model.ViewModel;
@@ -23,6 +26,7 @@ import fr.tolc.jahia.intellij.plugin.cnd.psi.CndNodeType;
 import fr.tolc.jahia.intellij.plugin.cnd.psi.CndProperty;
 import fr.tolc.jahia.intellij.plugin.cnd.utils.CndProjectFilesUtil;
 import fr.tolc.jahia.intellij.plugin.cnd.utils.CndUtil;
+import fr.tolc.jahia.intellij.plugin.cnd.utils.JspUtil;
 import fr.tolc.jahia.intellij.plugin.cnd.utils.PsiUtil;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
@@ -34,18 +38,19 @@ public class CndJspAnnotator implements Annotator {
     public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
         if (XmlElementType.XML_ATTRIBUTE_VALUE.equals(element.getNode().getElementType())) {
 
-            Set<PsiElement> elExpressions = PsiUtil.findFirstDescendantsByType(element, ELElementTypes.EL_SELECT_EXPRESSION, ELElementTypes.EL_SLICE_EXPRESSION);
-            for (PsiElement elExpression : elExpressions) {
-                String value = elExpression.getText();
+            if (JspUtil.isJcrNodePropertyName((XmlAttributeValue) element)) {
+                //<jcr:nodeProperty var="lol" node="${currentNode}" name="lol"/>
+                XmlAttributeValue attributeValue = (XmlAttributeValue) element;
 
-                Matcher matcher = propertyGetRegex.matcher(value);
-                while (matcher.find()) {
-                    String nodeVar = StringUtils.isNotBlank(matcher.group(1))? matcher.group(1) : matcher.group(3);
-                    String propertyName = StringUtils.isNotBlank(matcher.group(2))? matcher.group(2) : matcher.group(4);
+                String propertyName = attributeValue.getValue();
+                XmlAttribute nodeAttr = ((XmlTag) attributeValue.getParent().getParent()).getAttribute(JspUtil.TAG_ATTRIBUTE_NODE);
 
-                    int offset = elExpression.getTextRange().getStartOffset() + ((matcher.start(2) > -1)? matcher.start(2) : matcher.start(4));
+                if (nodeAttr != null) {
+                    String nodeVar = (nodeAttr.getValue() != null) ? nodeAttr.getValue().replaceAll("\\$\\{|}", "") : "";
+
+                    int offset = attributeValue.getTextRange().getStartOffset() + 1;
                     TextRange propertyRange = new TextRange(offset, offset + propertyName.length());
-                    
+
                     if (CURRENT_NODE.equals(nodeVar)) {
                         ViewModel viewModel = CndProjectFilesUtil.getViewModelFromPotentialViewFile(element.getContainingFile().getVirtualFile());
                         if (viewModel != null) {
@@ -79,6 +84,57 @@ public class CndJspAnnotator implements Annotator {
                             Annotation propertyAnnotation = holder.createErrorAnnotation(propertyRange,
                                     "Property '" + propertyName + "' not found in project or Jahia modules.\r\nAre you sure it exists?");
                             propertyAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.NUMBER);
+                        }
+                    }
+                }
+            } else {
+
+                Set<PsiElement> elExpressions = PsiUtil.findFirstDescendantsByType(element, ELElementTypes.EL_SELECT_EXPRESSION, ELElementTypes.EL_SLICE_EXPRESSION);
+                for (PsiElement elExpression : elExpressions) {
+                    String value = elExpression.getText();
+
+                    Matcher matcher = propertyGetRegex.matcher(value);
+                    while (matcher.find()) {
+                        String nodeVar = StringUtils.isNotBlank(matcher.group(1)) ? matcher.group(1) : matcher.group(3);
+                        String propertyName = StringUtils.isNotBlank(matcher.group(2)) ? matcher.group(2) : matcher.group(4);
+
+                        int offset = elExpression.getTextRange().getStartOffset() + ((matcher.start(2) > -1) ? matcher.start(2) : matcher.start(4));
+                        TextRange propertyRange = new TextRange(offset, offset + propertyName.length());
+
+                        if (CURRENT_NODE.equals(nodeVar)) {
+                            ViewModel viewModel = CndProjectFilesUtil.getViewModelFromPotentialViewFile(element.getContainingFile().getVirtualFile());
+                            if (viewModel != null) {
+                                CndNodeType nodeType = CndUtil.findNodeType(element.getProject(), viewModel.getNodeType());
+                                if (nodeType != null) {
+                                    CndProperty property = nodeType.getProperty(propertyName);
+                                    if (property != null) {
+                                        Annotation propertyAnnotation = holder.createInfoAnnotation(propertyRange, null);
+                                        propertyAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.NUMBER);
+                                    } else {
+                                        Set<CndProperty> possibleProperties = CndUtil.findProperties(element.getProject(), propertyName);
+                                        if (!possibleProperties.isEmpty()) {
+                                            Annotation propertyAnnotation = holder.createWarningAnnotation(propertyRange,
+                                                    "Property '" + propertyName + "' not found for node type '" + viewModel.getNodeType().toString() + "'.\r\nAre you sure it exists?");
+                                            propertyAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.NUMBER);
+                                        }
+                                    }
+                                } else {
+                                    Annotation propertyAnnotation = holder.createWarningAnnotation(propertyRange,
+                                            "Node type '" + viewModel.getNodeType().toString() + "' not found.\r\nAre you sure it exists?");
+                                    propertyAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.NUMBER);
+                                }
+                            }
+                        } else {
+                            //Get all properties with same name from project
+                            Set<CndProperty> possibleProperties = CndUtil.findProperties(element.getProject(), propertyName);
+                            if (!possibleProperties.isEmpty()) {
+                                Annotation propertyAnnotation = holder.createInfoAnnotation(propertyRange, null);
+                                propertyAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.NUMBER);
+                            } else {
+                                Annotation propertyAnnotation = holder.createErrorAnnotation(propertyRange,
+                                        "Property '" + propertyName + "' not found in project or Jahia modules.\r\nAre you sure it exists?");
+                                propertyAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.NUMBER);
+                            }
                         }
                     }
                 }
